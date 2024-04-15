10769b1b62bfd667fde0571039507d852dca3c4c
Dora Choi <135581648+dorachoii@users.noreply.github.com>
2024-04-15 15:01:14 +0900

    Initial Commit

---
+++ BlastDiaryFinal/BlastDiaryFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlastDiaryFinalApp.swift
+//  BlastDiaryFinal
+//
+//  Created by dora on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BlastDiaryFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
